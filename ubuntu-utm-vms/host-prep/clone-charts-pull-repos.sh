2e1242b0cbaed9080fcd1aa5e8c91d66a64b826d
Charles Federspiel <charlesfederspiel@Mac.attlocal.net>
2025-04-12 21:16:13 -0700

    deployed ldap w/ config secret no cert

--- ubuntu-utm-vms/host-prep/clone-charts-pull-repos.sh
+++ ubuntu-utm-vms/host-prep/clone-charts-pull-repos.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-INITIALDIR=$(dirname 0)
-BASEDIR=$INITIALDIR/charts
-mkdir -p $BASEDIR
-cd $BASEDIR
+INITIAL_DIR=$(dirname 0)
+REFERENCE_DIR=$INITIALDIR/host-prep/original-reference/
+
+mkdir -p $REFERENCE_DIR/charts $REFERENCE_DIR/gitrepos
+cd $REFERENCE_DIR/charts
 
 echo `pwd`
 
 k8sImages=$(docker image ls --filter=reference='registry.k8s.io/sig-storage/*' --format '{{.Repository}}:{{.Tag}}')
 
-MANIFESTDIR=$INITIALDIR/../guest/manifests/static/
+MANIFEST_DIR=$INITIAL_DIR/../guest/manifests/static/
 
 cd host-prep/original-reference/charts
 helm pull grafana/loki
@@ -21,5 +22,11 @@ mv operator minio-operator
 
 helm pull jetstack/cert-manager
 tar -xzf cert-manager-*
+
+cd ../gitrepos/
+git clone https://github.com/osixia/docker-openldap.git
+cp docker-openldap/example/kubernetes/using-secrets/gce-statefullset.yaml guest/manifests/static/ldap-ss.yaml
+cp docker-openldap/example/kubernetes/using-secrets/environment/my-env.yaml.example guest/manifests/static/ldap-env.yaml
+cp docker-openldap/example/kubernetes/using-secrets/environment/my-env.startup.yaml.example guest/manifests/static/ldap-env.startup.yaml
 
 cd ../../../
